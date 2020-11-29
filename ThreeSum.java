0671527f09686e6cddaa74222b1b029af8392fe0
Brian Meehan <bmeehan1020@gmail.com>
2020-11-29 13:51:27 -0500

    first commit

---
+++ ThreeSum.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ThreeSum (Medium)
+ * 
+ * Given an array nums of n integers, are there elements a, b, c in nums such
+ * that a + b + c = 0? Find all unique triplets in the array which gives the sum
+ * of zero. Notice that the solution set must not contain duplicate triplets.
+ */
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        return null;
+    }
+}
