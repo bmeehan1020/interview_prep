f0cfa54e94b5b1ebe2f4a0523a54aae16ccdfc03
Brian Meehan <meehanb1@wit.edu>
2021-02-11 08:31:45 -0500

    is_unique

---
+++ arrays_strings/is_unique.py
@@ -0,0 +1,24 @@
+# Solved? NO
+
+# CTCI 1.1
+# Implement algorithm to determine if string has all unique chars
+# What if you can't use additional DS?
+
+
+def is_unique(str):
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+
+    # Solutions
+
+    # Pythonic = Sets: if length of set of string is same as length of string O(n)
+    # Pythonic = (char_set = [False] * 128) create array quickly, index using ord()
+    # bit vector = stored in single variable, obj = 28 bytes * 8 = 224 bits > 128 (ASCII)
+    # dict = key: char, val: freq
